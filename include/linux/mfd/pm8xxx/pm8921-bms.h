8197f461cf99b9c76bf65d9830106fdf2806a2e9
Adam Farden <adam@farden.cz>
2013-09-21 19:41:17 +0200

    Update Battery and Charging driver

--- include/linux/mfd/pm8xxx/pm8921-bms.h
+++ include/linux/mfd/pm8xxx/pm8921-bms.h
@@ -14,6 +14,7 @@
 #define __PM8XXX_BMS_H
 
 #include <linux/errno.h>
+#include <linux/types.h>
 #include <linux/mfd/pm8xxx/batterydata-lib.h>
 
 #define PM8921_BMS_DEV_NAME	"pm8921-bms"
@@ -45,6 +46,7 @@ struct pm8xxx_bms_core_data {
 struct pm8921_bms_platform_data {
 	struct pm8xxx_bms_core_data	bms_cdata;
 	enum battery_type		battery_type;
+	struct bms_battery_data		*battery_data;
 	int				r_sense_uohm;
 	unsigned int			i_test;
 	unsigned int			v_cutoff;
@@ -92,6 +94,12 @@ int pm8921_bms_get_battery_current(int *result);
  *
  */
 int pm8921_bms_get_percent_charge(void);
+
+/**
+ * pm8921_bms_get_init_fcc - returns initial fcc in mAh of the battery
+ *
+ */
+int pm8921_bms_get_init_fcc(void);
 
 /**
  * pm8921_bms_get_fcc - returns fcc in mAh of the battery depending on its age
@@ -152,6 +160,10 @@ static inline int pm8921_bms_get_battery_current(int *result)
 	return -ENXIO;
 }
 static inline int pm8921_bms_get_percent_charge(void)
+{
+	return -ENXIO;
+}
+static inline int pm8921_bms_get_init_fcc(void)
 {
 	return -ENXIO;
 }
